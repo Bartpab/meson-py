0e078f3af65538276a96ba2c7798aa94b102cc1f
vcole <vcole@us.globalinfotek.com>
2023-09-11 09:42:33 -0400

    convert build method to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-import os
-from setuptools import setup, Command, find_packages
+from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+setup()
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
-
-setup(
-	name='MesonPy',
-	version = "0.0.1",
-	author = "Gael Pabois",
-	author_email = "gael.pabois@gmail.com",
-	packages = find_packages(),
-	url="https://github.com/Bartpab/meson-py",
-	license="LICENSE",
-	description="Websocket based python wrapper for Electron projects",
-	long_description=open('README.md').read(),
-	install_requires=required,
-	cmdclass={
-		'clean': CleanCommand,
-	}
-)
